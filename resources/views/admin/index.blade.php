4af2d5adb88adfe655c11c5429940f763ac12c5f
Emrul Hasan <imemrul@gmail.com>
2022-03-18 16:19:49 +0400

    user dashboard html create

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,6 +11,90 @@
         <div class="block-header">
             <div class="row">
                 {{-- User Role 1=Admin,2=Host,3-Guest --}}
+                @if(Auth::user()->roll_id ===  3)
+                <div class="col-xs-4">
+                    <div class="info-box bg-green hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">TOTAL Booking</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+                {{-- User Role 1=Admin,2=Host,3-Guest --}}
+                @if(Auth::user()->roll_id ===  3)
+                <div class="col-xs-4">
+                    <div class="info-box bg-yellow hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Reserv Booking</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+                {{-- User Role 1=Admin,2=Host,3-Guest --}}
+                @if(Auth::user()->roll_id ===  3)
+                <div class="col-xs-4">
+                    <div class="info-box bg-blue hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Active Booking</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+                {{-- User Role 1=Admin,2=Host,3-Guest --}}
+                @if(Auth::user()->roll_id ===  3)
+                <div class="col-xs-4">
+                    <div class="info-box bg-blue hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Support Ticket</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+                {{-- User Role 1=Admin,2=Host,3-Guest --}}
+                @if(Auth::user()->roll_id ===  3)
+                <div class="col-xs-4">
+                    <div class="info-box bg-red hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Open Ticket</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+                {{-- User Role 1=Admin,2=Host,3-Guest --}}
+                @if(Auth::user()->roll_id ===  3)
+                <div class="col-xs-4">
+                    <div class="info-box bg-green hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Solved Ticket</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+                {{-- User Role 1=Admin,2=Host,3-Guest --}}
                 @if(Auth::user()->roll_id === 1 || Auth::user()->roll_id === 2)
                 <div class="col-xs-4">
                     <div class="info-box bg-pink hover-expand-effect">
@@ -93,6 +177,7 @@
                 @endif
             </div>
         </div>
+        
         {{-- User Role 1=Admin,2=Host,3-Guest --}}
         @if(Auth::user()->roll_id === 1 || Auth::user()->roll_id === 2)
         <div class="row clearfix">
@@ -132,6 +217,26 @@
             </div>
         </div>
         @endif
+        <div class="row clearfix">
+            <div class="col-xs-12">
+                <div class="card">
+                    <div class="header bg-teal">
+                        <div class="row clearfix">
+                            <div class="col-xs-12">
+                                <h5 class="">Notice Board</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="body">
+                        <div class="row clearfix">
+                            <div class="col-xs-12">
+                                <marquee>Welcome to ClickvipPool Online Pool Booking System Portal</marquee>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 @endsection
--- storage/framework/views/c7f1866081b7aa915614aeba3b9763b0d536fb12.php
+++ storage/framework/views/c7f1866081b7aa915614aeba3b9763b0d536fb12.php
@@ -11,6 +11,90 @@
         <div class="block-header">
             <div class="row">
                 
+                <?php if(Auth::user()->roll_id ===  3): ?>
+                <div class="col-xs-4">
+                    <div class="info-box bg-green hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">TOTAL Booking</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                <?php endif; ?>
+                
+                <?php if(Auth::user()->roll_id ===  3): ?>
+                <div class="col-xs-4">
+                    <div class="info-box bg-yellow hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Reserv Booking</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                <?php endif; ?>
+                
+                <?php if(Auth::user()->roll_id ===  3): ?>
+                <div class="col-xs-4">
+                    <div class="info-box bg-blue hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Active Booking</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                <?php endif; ?>
+                
+                <?php if(Auth::user()->roll_id ===  3): ?>
+                <div class="col-xs-4">
+                    <div class="info-box bg-blue hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Support Ticket</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                <?php endif; ?>
+                
+                <?php if(Auth::user()->roll_id ===  3): ?>
+                <div class="col-xs-4">
+                    <div class="info-box bg-red hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Open Ticket</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                <?php endif; ?>
+                
+                <?php if(Auth::user()->roll_id ===  3): ?>
+                <div class="col-xs-4">
+                    <div class="info-box bg-green hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">perm_identity</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Solved Ticket</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">0</div>
+                        </div>
+                    </div>
+                </div>
+                <?php endif; ?>
+                
                 <?php if(Auth::user()->roll_id === 1 || Auth::user()->roll_id === 2): ?>
                 <div class="col-xs-4">
                     <div class="info-box bg-pink hover-expand-effect">
@@ -94,6 +178,7 @@
             </div>
         </div>
         
+        
         <?php if(Auth::user()->roll_id === 1 || Auth::user()->roll_id === 2): ?>
         <div class="row clearfix">
             <div class="col-xs-12">
@@ -132,6 +217,26 @@
             </div>
         </div>
         <?php endif; ?>
+        <div class="row clearfix">
+            <div class="col-xs-12">
+                <div class="card">
+                    <div class="header bg-teal">
+                        <div class="row clearfix">
+                            <div class="col-xs-12">
+                                <h5 class="">Notice Board</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="body">
+                        <div class="row clearfix">
+                            <div class="col-xs-12">
+                                <marquee>Welcome to ClickvipPool Online Pool Booking System Portal</marquee>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 <?php $__env->stopSection(); ?>
